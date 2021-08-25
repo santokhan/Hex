dff47421926efef058ebe1c1792e2fe6bd44c5ba
Santo Khan <santok54@gmail.com>
2021-08-25 14:27:48 +0600

    update v1.1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,55 @@
 {
+    function themeToggler() {
+        const toggler = document.querySelector("#themeToggler");
+        let istoggle = false;
+
+        function toggle() {
+            const savedMode = localStorage.getItem("data-theme");
+            if (savedMode) {
+                document.body.setAttribute("data-theme", savedMode);
+            }
+        } toggle();
+
+        function cngMode() {
+            if (istoggle === false) {
+                localStorage.setItem("data-theme", "dark");
+                toggler.innerHTML = `<i class="fas fa-sun"></i>`;
+                istoggle = true;
+            } else {
+                localStorage.setItem("data-theme", "light");
+                toggler.innerHTML = `<i class="fas fa-moon"></i>`;
+                istoggle = false;
+            }
+            toggle();
+        } cngMode();
+
+        toggler.addEventListener("click", cngMode);
+    } themeToggler();
+}
+
+{
+    function dropDown() {
+        const toggler = document.getElementById("navToggler");
+        const targetId = toggler.attributes["data-target"].value;
+        const target = document.getElementById(targetId);
+        let istoggle = false;
+        function showHide() {
+            if (!istoggle) {
+                istoggle = true;
+                target.classList.add("expand");
+                toggler.style.transform = "rotate(-45deg)";
+                console.log(target);
+            } else {
+                istoggle = false;
+                target.classList.remove("expand");
+                toggler.style.transform = "rotate(0deg)";
+            }
+        }
+        toggler.addEventListener("click", showHide);
+    } dropDown();
+}
+
+{
     function scrollFunc() {
         const btn = document.getElementById("scrollTopBtn");
         btn.style.display = "none";
@@ -19,6 +70,5 @@
             //console.log(btn);
         }
         btn.addEventListener("click", scroll);
-    }
-    scrollFunc();
+    } scrollFunc();
 }
