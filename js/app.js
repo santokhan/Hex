68eed6173b3de9ac16561f343b441faff776cfa6
Santo <santokhan1999@gmail.com>
2021-09-11 18:05:23 +0600

    app js

--- js/app.js
+++ js/app.js
@@ -176,6 +176,8 @@ function gridItemsData(props) {
   /**Here props is array index */
   const data = [
     { name: "Ecommerce Store", href: "./websites/trimmer/index.html", src: "https://live.staticflickr.com/65535/51439618692_e467012815_c.jpg", },
+    { name: "Fashion", href: "./websites/fashion-ui/index.html", src: "https://live.staticflickr.com/65535/51441114009_b68ba1515a_c.jpg", },
+    { name: "Painter", href: "./websites/colors/index.html", src: "https://live.staticflickr.com/65535/51441330750_ccee079c3c_c.jpg", },
     { name: "Race Store", href: "#", src: "https://live.staticflickr.com/65535/51440617983_3238a859e6_c.jpg", },
     { name: "Interior", href: "#", src: "https://live.staticflickr.com/65535/51441112794_65cfe57c44_c.jpg", },
     { name: "Theme Builder", href: "#", src: "https://live.staticflickr.com/65535/51440618558_37c5c3f71b_c.jpg", },
@@ -186,8 +188,6 @@ function gridItemsData(props) {
     { name: "Real State", href: "#", src: "https://live.staticflickr.com/65535/51440617833_62372d9e1a_c.jpg", },
     { name: "Portfolio", href: "#", src: "https://live.staticflickr.com/65535/51439617162_71c73ff00e_c.jpg", },
     { name: "Transport Agency", href: "./websites/tport/index.html", src: "https://live.staticflickr.com/65535/51440618648_2bdc3dacc3_c.jpg", },
-    { name: "Fashion", href: "./websites/fashion-ui/index.html", src: "https://live.staticflickr.com/65535/51441114009_b68ba1515a_c.jpg", },
-    { name: "Painter", href: "./websites/colors/index.html", src: "https://live.staticflickr.com/65535/51441330750_ccee079c3c_c.jpg", },
     // { name: "", href: "#", src: "", },
   ];
   if (typeof (props) === "number") {
